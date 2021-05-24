4a47be832a9fc16323b47d37ea80e6097df76bce
Kelmer <kelmerdromero@hotmail.com>
2021-05-24 18:18:55 -0500

    validacion en registro de usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,18 +11,28 @@ class UserController extends Controller
 
     public function create(Request $request)
     {
-        $user = new User();
-        $user->id_user = $request->number_document;
-        $user->name = $request->name;
-        $user->last_name = $request->last_name;
-        $user->email = $request->email;
-        $user->doc_type = $request->doc_type;
-        $user->password = Hash::make($request->password);
-        $user->save();
+        $result = (object) User::where('email', '=', $request->email)->get();
 
-        return response()->json([
-            'data' => $user,
-            'message' => 'resource created'
-        ], 201);
+        if (sizeof($result) != 0) {
+            return response()->json([
+                'result' => $result,
+                'error' => 'duplicated',
+                'message' => 'this user exist'
+            ], 201);
+        } else {
+            $user = new User();
+            $user->id_user = $request->number_document;
+            $user->name = $request->name;
+            $user->last_name = $request->last_name;
+            $user->email = $request->email;
+            $user->doc_type = $request->doc_type;
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            return response()->json([
+                'data' => $user,
+                'message' => 'resource created'
+            ], 201);
+        }
     }
 }
